2d02ae37234f60cbffb478d720949597d29aed04
FideleLesobre2022 <fidelelesobre@gmail.com>
2025-01-09 17:05:20 +0200

    Creating the VIEWMODEL for Notes(In here, we will now execute all the function from the DAO(UPSERT, DELETE, GET) using VIEWMODEL fetch methodology) ALSO using the scope to persist background actions

---
+++ Kumbuka/app/src/main/java/com/example/kumbuka/NoteViewModel.kt
@@ -0,0 +1,43 @@
+package com.example.kumbuka
+
+import android.app.Application
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.kumbuka.database.NoteDatabase
+import com.example.kumbuka.database.NoteEntity
+import kotlinx.coroutines.launch
+
+class NoteViewModel(context : Application) : AndroidViewModel(context) {
+
+    val dao = NoteDatabase.getDatabase(context).dao()
+
+    val selectedNotes = mutableStateListOf<NoteEntity>()
+
+    fun updateNote(note: NoteEntity){
+        viewModelScope.launch{
+            dao.updateNote(note)
+        }
+    }
+
+    fun deleteNotes(notes : List<NoteEntity>){
+        viewModelScope.launch{
+            dao.deleteNotes(notes)
+        }
+    }
+
+    fun getData() = dao.getNotes()
+
+    fun onChangeSelection(note : NoteEntity){
+        if(selectedNotes.contains(note)){
+            selectedNotes.remove(note)
+        }
+        else{
+            selectedNotes.add(note)
+        }
+    }
+
+    fun clearSelectedList(){
+        selectedNotes.clear()
+    }
+}
