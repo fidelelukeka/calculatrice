3dde2487d95fdac56eca76a5bc903ab93023a04d
FideleLesobre2022 <fidelelesobre@gmail.com>
2025-01-09 17:02:23 +0200

    Creating the Entity for Notes

--- Kumbuka/app/src/main/java/com/example/kumbuka/database/NoteEntity.kt
+++ Kumbuka/app/src/main/java/com/example/kumbuka/database/NoteEntity.kt
@@ -4,7 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity
-data class Note(
+data class NoteEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Int? = null,
     val title: String = "",
